5bdcf905c4a7b1e3ab5684bd155dd491cc577dc5
calllduck <kettin@binus.ac.id>
2026-06-07 00:09:05 +0700

    fix allowed hosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: process.env.PORT || 4173,
-    allowedHosts: ['all']
+    allowedHosts: ['makemeup.up.railway.app']
   },
   server: {
     host: '0.0.0.0',
